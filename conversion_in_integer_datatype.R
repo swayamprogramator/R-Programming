dadc04bc358f18abd4a1a2f989c88a0eb47581ea
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-06 08:17:18 +0530

    Create conversion_in_integer_datatype.R

---
+++ conversion_in_integer_datatype.R
@@ -0,0 +1,13 @@
+#converting datatype 
+#integer
+
+int1=as.integer(52.654)
+int1  #remove fractional part
+int2=as.integer(25-25i)
+int2  #remove imaginary part
+int3=as.integer(FALSE)
+int3
+int4=as.integer("hello")
+int4  #output NA
+int5=as.integer("453")
+int5
