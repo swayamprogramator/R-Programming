8ca1cbfcf9424891c3478fc31aeed176bc66e08d
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-06 08:17:06 +0530

    Create conversion_in_logical_datatype.R

---
+++ conversion_in_logical_datatype.R
@@ -0,0 +1,15 @@
+#converting datatype 
+#numeric
+log1<-as.logical(25)
+log1
+log2<-as.logical(25L)
+log2
+log3<-as.logical(0L)
+log3
+#give false only for 0
+log4<-as.logical(0+5i)
+log4
+log5<-as.logical("hjaghj")
+log5
+log6<-as.logical("566")
+log6
