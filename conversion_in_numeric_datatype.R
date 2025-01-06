093a9a166fb36accca8cff0eb5a78a7e94ef90a4
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-06 08:17:26 +0530

    Create conversion_in_numeric_datatype.R

---
+++ conversion_in_numeric_datatype.R
@@ -0,0 +1,12 @@
+#converting datatype 
+#numeric
+num1<-as.numeric(26L)
+num1
+num2<as.numeric(25-56i)
+num2
+num3<-as.numeric(TRUE)
+num3
+num4<-as.numeric("hello")
+num4   #output NA=Not Applicable
+num5<-as.numeric("1234")
+num5   #if string have only numbers
