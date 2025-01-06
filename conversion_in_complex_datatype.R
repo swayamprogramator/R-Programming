2058b6141d49f441aacef8b130df155339eda487
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-06 08:16:53 +0530

    Create conversion_in_complex_datatype.R

---
+++ conversion_in_complex_datatype.R
@@ -0,0 +1,12 @@
+#converting datatype 
+#complex
+comp1<-as.complex(26)
+comp1
+comp2<-as.complex(45L)
+comp2
+comp3<-as.complex(TRUE)
+comp3
+comp4<-as.complex("hello")
+comp4
+comp5<-as.complex("2456")
+comp5
