5cd0805afecf92bb0abc2e0380a9ba039379ab14
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-13 07:06:50 +0530

    Update current.R

--- current.R
+++
@@ -1,8 +0,0 @@
-#taking input from user
-
-name<-readline(prompt = "Enter your name : ")
-age<-readline(prompt = "Enter your age : ")
-age<-as.numeric(age)
-#cat("Hello",name,"Your age is",age)
-print(paste0("Hello my name is ",name," and my age is ",age))#join string without space
-#print(paste("Hello my name is",name,"and my age is",age))#join string with 1 space
