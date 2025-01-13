553038f4a7da830f8e6fd3c1b5059521c52a7552
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-13 07:06:06 +0530

    Create naming_of_vector_element.R

---
+++ naming_of_vector_element.R
@@ -0,0 +1,15 @@
+#Data Structures in R
+
+#Vectors
+
+#element of vectors are known as component
+#length()-tells us no. of element in vector
+#two types of vector-atomic vector and list
+
+#Vector Operation
+#naming of vector
+
+a<-c("ram","shyam","mohan")
+names(a)=c("x1","x2","x3")
+a
+a["x1"]
