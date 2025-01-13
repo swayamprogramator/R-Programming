5fe72721ef727d0a6e55a05d550eb2b3bbeea105
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-13 07:05:26 +0530

    Create creating_vector.R

---
+++ creating_vector.R
@@ -0,0 +1,17 @@
+#Data Structures in R
+
+#Vectors
+
+#element of vectors are known as component
+#length()-tells us no. of element in vector
+#two types of vector-atomic vector and list
+
+#creating atomic vector
+a<-c(3,4,5,6,1,2)
+a
+b<-3:10
+b
+c<-seq(1,10,by=3)
+c
+d<-seq(1,10,length.out = 8) #give evenly spread sequence of length 8
+d
