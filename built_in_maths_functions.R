d10da71a19c4a2688472ae76d06f54ff71fe81a0
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-13 07:05:01 +0530

    Create built_in_maths_functions.R

---
+++ built_in_maths_functions.R
@@ -0,0 +1,24 @@
+# built functions in R i.e predefined function
+
+#function related to math
+x<--5
+y<-5
+print(abs(x))
+print(sqrt(5))#we cant evaluate sqrt of negative value
+print(ceiling(-5.6))
+print(ceiling(5.6))
+print(floor(-5.6))
+print(floor(5.6))
+
+z<-c(1.5,5.6,8.4) #remove decimal digit
+print(trunc(z))
+
+print(sin(60))
+print(log(10))
+print(exp(5))
+
+a1<-c(1:10,45)
+print(sum(a1))
+print(max(a1))
+print(min(a1))
+print(range(a1))
