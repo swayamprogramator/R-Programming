eb9854a3b16917b00b0b2c69dc3e15d1b2d60541
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-23 13:24:03 +0530

    Update current.R

---
+++ current.R
@@ -0,0 +1,20 @@
+#2.5->Overwrite the second and fourth row elements of the sixth layers 1,3 and 5 of (4) with 99
+r_name<-c("r1","r2","r3","r4")
+c_name<-c("c1","c2")
+m_name<-c("m1","m2","m3","m4","m5","m6")
+#1
+x<-array(seq(4.8,0.1,length.out=4*2*6),dim = c(4,2,6),dimnames = list(r_name,c_name,m_name))
+
+#2
+y<-matrix(c(x[4,2,],x[1,2,]),nrow = 6, ncol = 2 )
+
+#3
+z<-array(rep(c(y[2,]),times=4),dim=c(2,2,2))
+
+#4
+new_array<-x[,,-6]
+
+new_array[c(2,4),2,c(1,3,5)]=-99
+print(new_array)
+
+
