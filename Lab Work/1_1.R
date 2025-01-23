57cfaafe290acc2a06103d24dfe822d085e257da
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-23 12:22:44 +0530

    Create 1_1.R

---
+++ Lab Work/1_1.R
@@ -0,0 +1,15 @@
+#1.1->Develop the R program for basic Mathematical computation- Square, Square root, exponential etc
+
+x<-16
+#Square
+print(x^2)
+print(x*X)
+print(x**2)
+#square root
+print(sqrt(x))
+print(sapply(x,sqrt))
+print(x^(1/2))
+#Exponential
+print(exp(x))
+print(x^3)
+print(x**3)
