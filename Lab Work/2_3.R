04791d535f20e3e70825bd3fd0a0bb878fe72753
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-23 13:20:35 +0530

    Create 2_3.R

---
+++ Lab Work/2_3.R
@@ -0,0 +1,13 @@
+#2.3->Use a Fourfold repetition of the second row of the matrix formed in (2) to fill a new array of dimension 2x2x2.
+r_name<-c("r1","r2","r3","r4")
+c_name<-c("c1","c2")
+m_name<-c("m1","m2","m3","m4","m5","m6")
+#1
+x<-array(seq(4.8,0.1,length.out=4*2*6),dim = c(4,2,6),dimnames = list(r_name,c_name,m_name))
+#2
+y<-matrix(c(x[4,2,],x[1,2,]),nrow = 6, ncol = 2 )
+
+z<-array(rep(c(y[2,]),times=4),dim=c(2,2,2))
+print(z)
+
+
