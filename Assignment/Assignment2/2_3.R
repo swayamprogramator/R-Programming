720c799e6cdef7e0616c402c9f08680f1d412180
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-31 08:30:13 +0530

    Create 2_3.R

---
+++ Assignment/Assignment2/2_3.R
@@ -0,0 +1,11 @@
+#3. Overwrite the second column of the matrix from (1.) with that same column sorted from smallest
+#   to largest.
+
+#1
+mat1<-matrix(c(4.3,3.1,8.2,8.2,3.2,0.9,1.6,6.5),nrow=4,ncol=2,byrow=TRUE)
+
+vec1<-mat1[,2]
+vec1<-sort(vec1)
+print(vec1)
+mat1[,2]<-vec1
+print(mat1)
