a8720d77630f9df878dd7c2a3cc09d6eedab0dfe
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-31 23:47:28 +0530

    Create 2_11.R

---
+++ Assignment/Assignment2/2_11.R
@@ -0,0 +1,12 @@
+#11->Extract and store as a new object the fourth and first row elements, in that order, of the second column only of all layers of (1).
+r_name<-c("r1","r2","r3","r4")
+c_name<-c("c1","c2")
+m_name<-c("m1","m2","m3","m4","m5","m6")
+
+#10
+x<-array(seq(4.8,0.1,length.out=4*2*6),dim = c(4,2,6),dimnames = list(r_name,c_name,m_name))
+
+y<-matrix(c(x[4,2,],x[1,2,]),nrow = 2, ncol = 6 )
+print(y)
+
+
