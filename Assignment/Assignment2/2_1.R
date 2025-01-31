dee27f3ca16030be0f4dc92b97cadcfd3ab3150f
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-31 08:21:27 +0530

    Create 2_1.R

---
+++ Assignment/Assignment2/2_1.R
@@ -0,0 +1,5 @@
+#1. Construct and store a 4 x 2 matrix that’s filled row-wise with the values 4.3, 3.1, 8.2, 8.2, 3.2,
+#    0.9, 1.6, and 6.5, in that order.
+
+mat1<-matrix(c(4.3,3.1,8.2,8.2,3.2,0.9,1.6,6.5),nrow=4,ncol=2,byrow=TRUE)
+print(mat1)
