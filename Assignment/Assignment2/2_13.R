eb79487a449972e70af26ec4b0a4dc6710e42ffc
Harsh Sharma <student3.ai@csvtu.ac.in>
2025-01-31 23:47:36 +0530

    Create 2_13.R

---
+++ Assignment/Assignment2/2_13.R
@@ -0,0 +1,11 @@
+#13->Create a new array comprised of the result of deleting the sixth layer of 1
+r_name<-c("r1","r2","r3","r4")
+c_name<-c("c1","c2")
+m_name<-c("m1","m2","m3","m4","m5","m6")
+#10
+x<-array(seq(4.8,0.1,length.out=4*2*6),dim = c(4,2,6),dimnames = list(r_name,c_name,m_name))
+
+new_array<-x[,,-6]
+print(new_array)
+
+
